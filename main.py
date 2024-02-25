ada95792909b942646986db88b8ffd5f6c0ae7b9
Chong <chongwan.w@gmail.com>
2024-02-24 21:05:36 -0500

    Small Changes

--- main.py
+++ main.py
@@ -4,10 +4,3 @@ from __future__ import annotations
 
 import csv
 from typing import Any, Optional
-
-class Tree:
-    """The tree class that is the backbone for the project"""
-    _root: Optional[Any]
-    _subtrees: list[Tree]
-
-    is_empty
